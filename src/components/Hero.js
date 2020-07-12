4fe6a73b55ebcc5ee62f74fa3f401d9cd25a8b32
kevin ahmad <kevinahmad.f@gmail.com>
2020-07-12 16:20:05 +0700

    finishing header

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import { Flex, Heading } from '@chakra-ui/core'
 
 export const Hero = ({ title }) => (
-  <Flex justifyContent="center" alignItems="center" height="100vh">
+  <Flex justifyContent="center" alignItems="center" height="80vh">
     <Heading fontSize="9vw">{title}</Heading>
   </Flex>
 )
