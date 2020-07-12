900f0c7151502d1ed6d77cc3c05f1ddbbcf3ba82
kevin ahmad <kevinahmad.f@gmail.com>
2020-07-12 16:22:32 +0700

    fix header again

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import { Flex, Heading } from '@chakra-ui/core'
 
 export const Hero = ({ title }) => (
-  <Flex justifyContent="center" alignItems="center" height="80vh">
+  <Flex justifyContent="center" alignItems="center" height="90vh">
     <Heading fontSize="9vw">{title}</Heading>
   </Flex>
 )
